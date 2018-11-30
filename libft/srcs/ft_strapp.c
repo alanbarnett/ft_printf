5cfe86cc273624063df4a3f3af5d09f266241a1c
Alan Barnett <alanbarnett328@gmail.com>
2018-11-30 04:42:39 -0800

    added a comment explaining function usage

--- libft/srcs/ft_strapp.c
+++ libft/srcs/ft_strapp.c
@@ -6,12 +6,17 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/07 20:38:30 by abarnett          #+#    #+#             */
-/*   Updated: 2018/06/09 14:19:59 by abarnett         ###   ########.fr       */
+/*   Updated: 2018/11/29 01:41:31 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** This function appends a character to a string by changing the null terminator
+** to a character, and then setting a new null terminator after the character.
+** It is assumed that you have enough memory space for this.
+*/
 void	ft_strapp(char *dst, char c)
 {
 	if (!dst)
