85a8780a96aff8acdfce01361ae0cb9b6eb89ae9
Alan Barnett <alanbarnett328@gmail.com>
2018-12-04 18:41:46 -0800

    adding bunch of test cases, and organizing into sections

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: alan </var/spool/mail/alan>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/28 20:15:25 by alan              #+#    #+#             */
-/*   Updated: 2018/11/29 00:12:12 by alan             ###   ########.fr       */
+/*   Updated: 2018/12/04 15:58:34 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,78 @@
 
 int	main(void)
 {
-	//int		i;
+	int		i;
 
-	//i = 99;
-	//ft_printf("Hello, World!\n");
-	ft_printf("Hello %.5s!\n", "world, and all it's inhabitants");
-	//ft_printf("testing a %char\n", (char)i);
-	//ft_printf("i: %d\n", i);
-	//ft_printf("int: %+015.7d, int2: %-0015.8d, testing some %5% shits % %\n", i, 17);
-	//ft_printf("testing with %lld %hhd %ld %hd\n", i, i, i, i);
-	//ft_printf("%# 05.3d %.d %d %d gg!\n", 1, -2, 33, 42);
+	i = 99;
 	/*
+	// normal tests
+	ft_printf("Hello, World!\n");
+	ft_printf("testing a %char\n", (char)i);
+	ft_printf("i: %d\n", i);
+	ft_printf("String: \"%s\"\nInt: %d\n", "stringy string", 1990);
+	*/
+	/*
+	// tests with precision and width for strings and ints
+	ft_printf("Hello %.5s!\n", "world, and all it's inhabitants");
+	ft_printf("Hello %.10s!\n", "world");
+	ft_printf("|%10s|, hello\n", "hello");
+	ft_printf("|%10.3s|, hello\n", "hello");
+	ft_printf("|%10.9s|, hello\n", "hello");
+	ft_printf("|%10c|, h\n", 'h');
+	ft_printf("|%10%|\n");
+	ft_printf("|%-10s|, helo\n", "hello");
+	ft_printf("|%-10.3s|, helo\n", "hello");
+	ft_printf("|%-10.9s|, helo\n", "hello");
+	ft_printf("|%-10c|, h\n", 'h');
+	ft_printf("|%-10%|\n");
+	ft_printf("|%.5d|, 99\n", i);
+	ft_printf("|%.0d|, 99\n", i);
+	ft_printf("|%.d|, 0\n", 0);
+	ft_printf("|%7.5d|, 99\n", i);
+	ft_printf("|%7.0d|, 99\n", i);
+	ft_printf("|%7.d|, 0\n", 0);
+	ft_printf("|%-7.5d|, 99\n", i);
+	ft_printf("|%0+7.5d|, 99\n", i);
+	ft_printf("|% 07.0d|, 99\n", i);
+	ft_printf("|%-07.d|, 0\n", 0);
+	*/
+	/*
+	// testing 0 flag for ints
+	ft_printf("|%08d|\n", i);
+	ft_printf("|%02d|\n", i);
+	ft_printf("|%01d|\n", i);
+	ft_printf("|%08.5d|\n", i);
+	ft_printf("|%-8.5d|\n", i);
+	ft_printf("|%08.5d|\n", -i);
+	ft_printf("|%-8.5d|\n", -i);
+	ft_printf("|%8.9d|\n", -i);
+	ft_printf("|%-8.9d|\n", i);
+	ft_printf("|%02.d|\n", i);
+	ft_printf("|%01.d|\n", 0);
+	ft_printf("|%08d|\n", -i);
+	ft_printf("|%08.5d|\n", -i);
+	ft_printf("|%.5d|, 99\n", i);
+	ft_printf("|%.0d|, 99\n", i);
+	ft_printf("|%.d|, 0\n", 0);
+	ft_printf("|%7.5d|, 99\n", i);
+	ft_printf("|%7.0d|, 99\n", i);
+	ft_printf("|%7.d|, 0\n", 0);
+	ft_printf("|%-7.5d|, 99\n", i);
+	ft_printf("|%0+7.5d|, 99\n", i);
+	ft_printf("|% 07.0d|, 99\n", i);
+	ft_printf("|%-07.d|, 0\n", 0);
+	*/
+	/*
+	// testing lengths
+	ft_printf("testing with %lld %hhd %ld %hd\n", i, i, i, i);
+	*/
+	/*
+	// gross tests
+	ft_printf("%%10% : |%10%|\n");
+	*/
+
+	/*
+	// testing standard printf
 	printf("%10.d\n", i);
 	printf("%p\n", (void *)0);
 	printf("%zD\n", (long)i);
@@ -36,7 +97,7 @@ int	main(void)
 	printf("string: %10s\n", "hello");
 	printf("%X\n", 12648430);
 	printf("@moulitest: %s\n", NULL);
-	//printf("%U\n", 4294967296);
+	printf("%U\n", 4294967296);
 	printf("%hd\n", (short)32768);
 	printf("%hd\n", (short)-32769);
 	printf("%hhd\n", (char)-129);
@@ -51,9 +112,9 @@ int	main(void)
 	printf("%012.6d\n", -224);
 	printf("%12.6d\n", 224);
 	printf("%12.6d\n", -224);
-	//printf("% x\n", 0xC0FFEE);
+	printf("% x\n", 0xC0FFEE);
 	printf("int: %d, percent: |%5%\n", i);
-	//printf("%lu\n", sizeof(signed char));
+	printf("%lu\n", sizeof(signed char));
 	printf("%-6d\n", i);
 	printf("%+-6d\n", i);
 	printf("%06d\n", i);
@@ -68,35 +129,66 @@ int	main(void)
 	printf("% 010d\n", 135);
 	*/
 
-	//ft_printf("String: \"%s\"\nInt: %d\n", str, i);
-	/*
-	ft_printf("2d:    |%2d\n", i);
-	ft_printf("-2d:   |%-2d\n", i);
-	ft_printf("02d:   |%02d\n", i);
-	ft_printf("+2d:   |%+2d\n", i);
-	ft_printf("+07d:  |%+07d\n", i);
-	ft_printf("10d:   |%10d\n", i);
-	ft_printf("-10d:  |%-10d\n", i);
-	ft_printf("-d:    |%-d\n", i);
-	ft_printf("+d:    |%+d\n", i);
-	ft_printf("+10d:  |%+10d\n", i);
-	ft_printf("+010d: |%+010d\n", i);
-	ft_printf("+-10d: |%+-10d\n", i);
+	//ft_printf("int: %+015.7d, int2: %-0015.8d, testing some %5% shits % %\n", i, 17);
+	//ft_printf("%# 05.3d %.d %d %d gg!\n", 1, -2, 33, 42);
+
+	// versus test with width, precision, and -+ 0 flags
+	ft_printf("3d:      |%3d|\n", 42890);
+	ft_printf("3d:      |%3d|\n", -42890);
+	ft_printf("2d:      |%2d|\n", i);
+	ft_printf("-2d:     |%-2d|\n", i);
+	ft_printf("02d:     |%02d|\n", i);
+	ft_printf("+2d:     |%+2d|\n", i);
+	ft_printf(".4d:     |%.4d|\n", i);
+	ft_printf("-.4d:    |%-.4d|\n", i);
+	ft_printf("+.4d:    |%+.4d|\n", i);
+	ft_printf("+.4d:    |%+.4d|\n", -i);
+	ft_printf("+07d:    |%+07d|\n", i);
+	ft_printf("10d:     |%10d|\n", i);
+	ft_printf("-10d:    |%-10d|\n", i);
+	ft_printf("-d:      |%-d|\n", i);
+	ft_printf("+d:      |%+d|\n", i);
+	ft_printf("+10d:    |%+10d|\n", i);
+	ft_printf("+10d:    |%+10d|\n", -i);
+	ft_printf("+010d:   |%+010d|\n", i);
+	ft_printf("+010d:   |%+010d|\n", -i);
+	ft_printf("+-10d:   |%+-10d|\n", i);
+	ft_printf("-10.4d:  |%-10.4d|\n", i);
+	ft_printf("10.4d:   |%10.4d|\n", i);
+	ft_printf("-10.4d:  |%-10.4d|\n", -i);
+	ft_printf("10.4d:   |%10.4d|\n", -i);
+	ft_printf("-010.4d: |%-10.4d|\n", i);
+	ft_printf("+10.4d:  |%10.4d|\n", i);
+	ft_printf("-010.4d: |%-10.4d|\n", -i);
+	ft_printf("010.4d:  |%10.4d|\n", -i);
 	ft_putchar('\n');
-	printf("2d:    |%2d\n", i);
-	printf("-2d:   |%-2d\n", i);
-	printf("02d:   |%02d\n", i);
-	printf("+2d:   |%+2d\n", i);
-	printf("+07d:  |%+07d\n", i);
-	printf("10d:   |%10d\n", i);
-	printf("-10d:  |%-10d\n", i);
-	printf("-d:    |%-d\n", i);
-	printf("+d:    |%+d\n", i);
-	printf("+10d:  |%+10d\n", i);
-	printf("+10d:  |%+10d\n", -i);
-	printf("+010d: |%+010d\n", i);
-	printf("+010d: |%+010d\n", -i);
-	printf("+-10d: |%+-10d\n", i);
-	*/
+	printf("3d:      |%3d|\n", 42890);
+	printf("3d:      |%3d|\n", -42890);
+	printf("2d:      |%2d|\n", i);
+	printf("-2d:     |%-2d|\n", i);
+	printf("02d:     |%02d|\n", i);
+	printf("+2d:     |%+2d|\n", i);
+	printf(".4d:     |%.4d|\n", i);
+	printf("-.4d:    |%-.4d|\n", i);
+	printf("+.4d:    |%+.4d|\n", i);
+	printf("+.4d:    |%+.4d|\n", -i);
+	printf("+07d:    |%+07d|\n", i);
+	printf("10d:     |%10d|\n", i);
+	printf("-10d:    |%-10d|\n", i);
+	printf("-d:      |%-d|\n", i);
+	printf("+d:      |%+d|\n", i);
+	printf("+10d:    |%+10d|\n", i);
+	printf("+10d:    |%+10d|\n", -i);
+	printf("+010d:   |%+010d|\n", i);
+	printf("+010d:   |%+010d|\n", -i);
+	printf("+-10d:   |%+-10d|\n", i);
+	printf("-10.4d:  |%-10.4d|\n", i);
+	printf("10.4d:   |%10.4d|\n", i);
+	printf("-10.4d:  |%-10.4d|\n", -i);
+	printf("10.4d:   |%10.4d|\n", -i);
+	printf("-010.4d: |%-10.4d|\n", i);
+	printf("+10.4d:  |%10.4d|\n", i);
+	printf("-010.4d: |%-10.4d|\n", -i);
+	printf("010.4d:  |%10.4d|\n", -i);
 	return (0);
 }
