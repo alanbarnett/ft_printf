f3be3313e34a32b9827de639a9c3306ec3957d35
Alan Barnett <alanbarnett328@gmail.com>
2018-12-04 18:41:12 -0800

    moved the comment to be touching the function. just looks better this way.

--- libft/srcs/ft_nmemset.c
+++ libft/srcs/ft_nmemset.c
@@ -6,17 +6,17 @@
 /*   By: abarnett <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/23 16:35:35 by abarnett          #+#    #+#             */
-/*   Updated: 2018/06/02 17:01:34 by abarnett         ###   ########.fr       */
+/*   Updated: 2018/12/02 19:29:18 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+
+#include "libft.h"
 
 /*
 ** An implementation of memset that does not segfault if b is NULL
 ** (Returns NULL instead)
 */
-
-#include "libft.h"
-
 void	*ft_nmemset(void *b, int c, size_t len)
 {
 	unsigned char	*cur;
