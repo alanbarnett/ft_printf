3bb24788450b21b07c06b4572caab1ddf9573114
Alan Barnett <alanbarnett328@gmail.com>
2018-12-10 20:00:42 -0800

    Add ft_lstputstr_len function

--- libft/srcs/ft_lstputstr.c
+++ libft/srcs/ft_lstputstr.c
@@ -6,7 +6,7 @@
 /*   By: alan </var/spool/mail/alan>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/26 17:02:34 by alan              #+#    #+#             */
-/*   Updated: 2018/11/26 17:22:12 by alan             ###   ########.fr       */
+/*   Updated: 2018/12/10 19:43:23 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,14 @@
 void				ft_lstputstr(t_list *item)
 {
 	if (item)
-		ft_putstr((char *)item->content);
+		write(1, (char *)item->content, ft_strlen(item->content));
+}
+
+/*
+** Same as above, except it will print a specific length
+*/
+void				ft_lstputstr_len(t_list *item)
+{
+	if (item)
+		write(1, (char *)item->content, item->content_size);
 }
