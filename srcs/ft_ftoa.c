dce3ada3eafd856f9d8372703359a329560667da
Alan Barnett <alanbarnett328@gmail.com>
2018-12-20 03:35:28 -0800

    Redo fraction collection

--- srcs/ft_ftoa.c
+++ srcs/ft_ftoa.c
@@ -6,7 +6,7 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 23:37:04 by alan              #+#    #+#             */
-/*   Updated: 2018/12/20 01:11:51 by alan             ###   ########.fr       */
+/*   Updated: 2018/12/20 03:24:41 by alan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,106 +47,55 @@ static void		ft_nbrcpy_p(long nb, int precision, char *str)
 		*str = (nb % 10 + '0');
 }
 
-static long		get_fraction(int exp, long mantissa, int precision)
+static void		copy_fraction(char *str, double num, int precision, int offset)
 {
-	int			index;
-	int			max;
-	t_double	multiple;
-	double		fraction;
+	long	fraction;
+	int		len_of_f;
 
-	// index is the bit we're looking at, starting at one
-	index = 1;
-	// add to index the negative offset from exponent, to get aligned with the
-	// front of the mantissa
-	// If the fractional part is not just zero (by the fact that the mantissa
-	// exists), then subtract 1 from the index to look at the previous location
-	// to get the implied 1 that has already been placed
-	if (exp < 0)
-		index += -(exp) - (mantissa ? 1 : 0);
-	// maybe subtract precision in this equation by some expression with
-	// exponent, if it's negative. after a certain amount I won't be affecting
-	// many numbers at all, and this would allow for very small exponents
-	multiple.d = (double)ft_pow(10, precision);
-	/*
-	// Max index is ((precision * 3) + (precision / 3))
-	// if the number is not a multiple of 3, add 1
-	// 3 adds 1, 4 adds 2
-	// 9 adds 3, 10 adds 4
-	//
-	// 1, 1
-	// 2, 1
-	// 3, 1
-	// 4, 2
-	// 5, 2
-	// 6, 2
-	// 7, 3
-	// 8, 3
-	// 9, 3
-	// 10,4
-	//
-	// ceil(precision / 3)
-	// adding one to precision to get better rounding behavior
-	*/
-	max = ((precision + 1) * 3) + ft_ceil((double)((precision + 1) / 3));
-	fraction = 0;
-	// max is the number of times starting at 1,
-	// index is the number of times starting at 1,
-	// index should go up to and including max
-	while (index <= max)
+	len_of_f = 0;
+	while (len_of_f < precision && len_of_f < 17)
 	{
-		// to isolate the bit, we take 52 (highest bit in mantissa part), minus
-		// exponent (to offset the intpart of the binary scientific notation),
-		// minus index (to step down the mantissa)
-		if (mantissa & (1L << ((52 - exp) - index)))
-			fraction += multiple.d / (1L << index);
-		++index;
+		num *= 10;
+		++len_of_f;
 	}
-
-	return(ft_round(fraction));
+	fraction = ft_round(num);
+	ft_nbrcpy_p(fraction, len_of_f, str + offset + len_of_f); 
+	if (precision > len_of_f)
+		ft_nbrcpy_p(0, precision - len_of_f, str + offset + precision);
 }
 
-static char		*make_string(int sign, int exp, long mantissa, int precision)
+static char		*make_string(int sign, long intpart, double nb, int precision)
 {
-	long	intpart;
-	int		len_of_intpart;
-	long	fraction;
+	int		len_of_i;
 	char	*str;
 
-	intpart = 0;
-	if (exp > 0)
-		intpart = (mantissa >> (52 - exp));
-	len_of_intpart = ft_numlen(intpart);
-	fraction = get_fraction(exp, mantissa, precision);
+	len_of_i = ft_numlen(intpart);
 	if (sign)
 	{
-		++len_of_intpart;
-		str = ft_strnew(len_of_intpart + 1 + precision);
+		++len_of_i;
+		str = ft_strnew(len_of_i + 1 + precision);
 		*str = '-';
-		ft_nbrcpy_p(intpart, len_of_intpart - 1, str + len_of_intpart - 1);
+		ft_nbrcpy_p(intpart, len_of_i - 1, str + len_of_i - 1);
 	}
 	else
 	{
-		str = ft_strnew(len_of_intpart + 1 + precision);
-		ft_nbrcpy_p(intpart, len_of_intpart, str + len_of_intpart - 1);
+		str = ft_strnew(len_of_i + 1 + precision);
+		ft_nbrcpy_p(intpart, len_of_i, str + len_of_i - 1);
 	}
-	str[len_of_intpart] = '.';
-	ft_nbrcpy_p(fraction, precision, str + len_of_intpart + precision); 
+	str[len_of_i] = '.';
+	copy_fraction(str, nb, precision, len_of_i);
 	return (str);
 }
 
 char			*ft_ftoa(double nb, int precision)
 {
 	t_double	unb;
-	int			sign;
 	int			exp;
 	long		mantissa;
-	char		*str;
+	long		intpart;
 
 	unb.d = nb;
 
-	// and selects only the first bit for the sign
-	// !! will turn it off and back on, setting it to 0 or 1
-	sign = !!(unb.l & 0x8000000000000000);
 	// shift by 52 to isolate the exponent, and to isolate exponent, minus bias
 	exp = ((unb.l >> 52) & 0x7ff) - 1023;
 	// and to isolate mantissa
@@ -154,7 +103,13 @@ char			*ft_ftoa(double nb, int precision)
 
 	if (exp != 0x7FF && !(exp == 0 && mantissa == 0))
 		mantissa |= (1L << 52);
-	str = make_string(sign, exp, mantissa, precision);
-
-	return (str);
+	intpart = 0;
+	if (exp > 0)
+	{
+		if (exp > 52)
+			intpart = (mantissa << (exp - 52));
+		else
+			intpart = (mantissa >> (52 - exp));
+	}
+	return (make_string((unb.d < 0), intpart, unb.d, precision));
 }
