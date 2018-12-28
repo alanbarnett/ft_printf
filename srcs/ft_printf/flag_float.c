5b726b94d3f8f78b4eabcac927edc705d8a4b8f5
Alan Barnett <abarnett@e1z2r4p9.42.us.org>
2018-12-28 01:41:39 -0800

    Add floating point flag handler

---
+++ srcs/ft_printf/flag_float.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   flag_float.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/15 14:24:01 by alan              #+#    #+#             */
+/*   Updated: 2018/12/28 01:22:10 by abarnett         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libftprintf.h"
+
+static double	get_nb(char length, va_list valist)
+{
+	double	nb;
+
+	nb = 0;
+	if (length == 0)
+		nb = va_arg(valist, double);
+	else if (length == 'l')
+		nb = va_arg(valist, double);
+	else if (length == 'L')
+		nb = va_arg(valist, long double);
+	return (nb);
+}
+
+char			*flag_float(t_format *fmt, va_list valist)
+{
+	double	nb;
+	char	*str;
+
+	nb = get_nb(fmt->length, valist);
+	if (fmt->precision == -1)
+		fmt->precision = 6;
+	str = ft_ftoa(nb, fmt->precision);
+	fmt->width = ft_strlen(str);
+	return (str);
+}
