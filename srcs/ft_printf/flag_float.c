3a07af649cd692a5af4577f120f41a6323df07c7
Alan Barnett <abarnett@e1z2r4p9.42.us.org>
2019-01-02 21:18:29 -0800

    Add width functionality
    I added the addspaces function from unicode, and made the flag handler
    use it if the given width is greater than the size of the string after
    converting.

--- srcs/ft_printf/flag_float.c
+++ srcs/ft_printf/flag_float.c
@@ -6,11 +6,33 @@
 /*   By: alan <alanbarnett328@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 14:24:01 by alan              #+#    #+#             */
-/*   Updated: 2018/12/29 23:43:01 by abarnett         ###   ########.fr       */
+/*   Updated: 2019/01/02 21:10:12 by abarnett         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+
+/*
+** This function adds extra spaces on the left or right size of a string, as
+** specified by the variable "left". If zero, spaces are put on the right side.
+** If 1, they are put on the left. The "spaces" variable holds the number of
+** extra spaces to be added.
+*/
+
+static char		*addspaces(char **str, int spaces, int left)
+{
+	int		len_str;
+	char	*newstr;
+
+	len_str = ft_strlen(*str);
+	newstr = ft_strinit(len_str + spaces, ' ');
+	if (left)
+		ft_strncpy(newstr, *str, len_str);
+	else
+		ft_strncpy(newstr + spaces, *str, len_str);
+	ft_strdel(str);
+	return (newstr);
+}
 
 static double	get_nb(char length, va_list valist)
 {
@@ -30,11 +52,15 @@ char			*flag_float(t_format *fmt, va_list valist)
 {
 	double	nb;
 	char	*str;
+	int		len;
 
 	nb = get_nb(fmt->length, valist);
 	if (fmt->precision == -1)
 		fmt->precision = 6;
 	str = ft_ftoa(nb, fmt->precision);
+	len = ft_strlen(str);
+	if (fmt->width > len)
+		str = addspaces(&str, fmt->width - len, fmt->flags & MINUS);
 	fmt->width = ft_strlen(str);
 	return (str);
 }
